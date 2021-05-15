61b219b3b550a2d18ea43ca9c2f4eda89453eb8a
Ken Smith <k.w.smith@qub.ac.uk>
2021-05-15 11:07:33 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+moduleDirectory = os.path.dirname(os.path.realpath(__file__))
+
+def readme():
+    with open(moduleDirectory + '/README.md') as f:
+        return f.read()
+
+setup(
+    name="psat-server-web",
+    description='Pan-STARRS and ATLAS web interface.',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    version="0.0.1",
+    author='genghisken',
+    author_email='ken.w.smith@gmail.com',
+    license='MIT',
+    url='https://github.com/genghisken/psat-server-web',
+    packages=find_packages(),
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Utilities',
+    ],
+    install_requires=[
+          'django',
+          'django_tables2',
+          'pyyaml',
+          'docopt',
+          'gkhtm',
+      ],
+    python_requires='>=3.6',
+    include_package_data=True,
+    zip_safe=False
+)
