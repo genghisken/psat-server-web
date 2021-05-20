aa5b0463f97ca08a23d8513a48c26d180a692ea6
Ken Smith <k.w.smith@qub.ac.uk>
2021-05-20 16:51:19 +0100

    Added instructions for which variables should be set.

--- psat_server_web/atlas/generate_mod_wsgi_apachectl.sh
+++ psat_server_web/atlas/generate_mod_wsgi_apachectl.sh
@@ -2,6 +2,28 @@
 # Create a setup script and immediately start the apache instance.  Our URL prefix
 # is specified by the --mount-point setting.  We need to specify a PYTHONPATH before
 # starting the apache instance. Run this script from THIS directory.
+# 
+# REQUIREMENTS:
+# Write a file in ~/.config/django/django_env_$CONDA_DEFAULT_ENV (e.g. atlas)
+# The contents of this file should be as follows:
+# export DJANGO_SECRET_KEY=''
+# 
+# export DJANGO_MYSQL_DBUSER=''
+# export DJANGO_MYSQL_DBPASS=''
+# export DJANGO_MYSQL_DBNAME=''
+# export DJANGO_MYSQL_DBHOST=''
+# export DJANGO_MYSQL_DBPORT=''
+# 
+# export DJANGO_TNS_DAEMON_PORT=''
+# export DJANGO_TNS_DAEMON_SERVER=''
+# export DJANGO_MPC_DAEMON_PORT=''
+# export DJANGO_MPC_DAEMON_SERVER=''
+# export DJANGO_NAME_DAEMON_PORT=''
+# export DJANGO_NAME_DAEMON_SERVER=''
+# 
+# export WSGI_PORT=''
+# export WSGI_PREFIX=''
+
 
 if [ -f ~/.config/django/django_env_$CONDA_DEFAULT_ENV ]; then chmod 600 ~/.config/django/django_env_$CONDA_DEFAULT_ENV; source ~/.config/django/django_env_$CONDA_DEFAULT_ENV; fi
 
