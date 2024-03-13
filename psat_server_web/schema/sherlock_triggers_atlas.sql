645c17bd5bebceb05a352c5c9d25386b9b765945
Ken Smith <k.w.smith@qub.ac.uk>
2024-03-13 18:25:35 +0000

    Added the sherlock triggers.

---
+++ psat_server_web/schema/sherlock_triggers_atlas.sql
@@ -0,0 +1,19 @@
+DELIMITER $$
+CREATE TRIGGER `sherlock_classifications_AFTER_INSERT` AFTER INSERT ON `sherlock_classifications` FOR EACH ROW
+BEGIN
+    update `atlas_diff_objects` set `sherlockClassification` = new.classification
+                        where `id`  = new.transient_object_id;
+END;
+$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER `sherlock_classifications_BEFORE_INSERT` BEFORE INSERT ON `sherlock_classifications` FOR EACH ROW
+BEGIN
+    IF new.classification = "ORPHAN" THEN
+        SET new.annotation = "The transient location is not matched against any known catalogued source", new.summary = "No catalogued match";
+    END IF;
+END;
+$$
+DELIMITER ;
+
